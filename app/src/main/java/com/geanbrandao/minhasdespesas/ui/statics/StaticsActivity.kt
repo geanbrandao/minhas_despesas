899369d571dfcad5705b2a4c7b5c5db4322f39f9
gean brandao <karlos867gean@gmail.com>
2020-10-18 17:19:24 -0300

    new library

---
+++ app/src/main/java/com/geanbrandao/minhasdespesas/ui/statics/StaticsActivity.kt
@@ -0,0 +1,13 @@
+package com.geanbrandao.minhasdespesas.ui.statics
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.geanbrandao.minhasdespesas.R
+import com.geanbrandao.minhasdespesas.ui.base.activity.BaseActivity
+
+class StaticsActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_statics)
+    }
+}
